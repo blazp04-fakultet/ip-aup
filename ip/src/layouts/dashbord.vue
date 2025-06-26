e4ccc30339ff11b6ea7e10b44db256cd37b6affa
blazp04 <blaz.peric4@gmail.com>
2025-06-26 05:47:28 +0200

    .

--- ip/src/layouts/dashbord.vue
+++ ip/src/layouts/dashbord.vue
@@ -1,36 +1,38 @@
 <template>
   <v-app id="inspire">
-
     <v-navigation-drawer v-model="drawer">
       <v-sheet class="pa-4" color="grey-darken-5">
-        <div>blaz@blazperic.com</div>
+        <div>LLMBridge</div>
       </v-sheet>
 
       <v-divider />
 
       <v-list>
-        <v-list-item v-for="[icon, text, route] in links" :key="icon" link :prepend-icon="icon" :title="text"
-          :to="route" />
+        <v-list-item
+          v-for="[icon, text, route] in links"
+          :key="icon"
+          link
+          :prepend-icon="icon"
+          :title="text"
+          :to="route"
+        />
       </v-list>
     </v-navigation-drawer>
 
     <v-main>
-
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
 const links = [
-  ['mdi-home', 'Home', '/home'],
-  ['mdi-key-variant', 'Api key', '/token'],
-  ['mdi-cash', 'Billing', '/billing'],
+  ["mdi-home", "Home", "/home"],
+  ["mdi-key-variant", "Api key", "/token"],
+  ["mdi-cash", "Billing", "/billing"],
+];
 
-
-]
-
-const drawer = ref<boolean>(true)
+const drawer = ref<boolean>(true);
 </script>
